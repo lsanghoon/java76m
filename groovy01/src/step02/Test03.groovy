138c1b908e2d7de57f347b868c1af868d4adbc6e
isanghoon <isanghoon@localhost>
2015-11-17 10:33:09 +0900

    .

---
+++ groovy01/src/step02/Test03.groovy
@@ -0,0 +1,19 @@
+//주제: 자바 변수를 선언할 수 있다.
+package step02
+
+// groovy 기본 임포트 패키지
+/* import java.lang.*
+ * import java.util.*
+ * import java.io.*
+ * import java.net.*
+ * import groovy.lang.*
+ * import groovy.util.*
+ * import
+ * 
+ */
+
+java.util.Date today = new java.util.Date();
+Date today2 = new Date();
+
+println today
+println today2
