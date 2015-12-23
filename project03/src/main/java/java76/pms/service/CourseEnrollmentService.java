cd618f94ee532ed23d9500b8d4f88318fb51eea9
lsanghoon <isanghoon@localhost>
2015-12-23 12:51:50 +0900

    .

--- project03/src/main/java/java76/pms/service/CourseEnrollmentService.java
+++ project03/src/main/java/java76/pms/service/CourseEnrollmentService.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java76.pms.dao.CourseEnrollmentDao;
 import java76.pms.domain.CourseEnrollment;
@@ -14,49 +18,62 @@ import java76.pms.domain.Student;
 public class CourseEnrollmentService {
 	@Autowired StudentService studentService;
 	@Autowired CourseEnrollmentDao enrollDao;
-	
+
+	@Autowired PlatformTransactionManager txManager;
+
 	public void enroll(CourseEnrollment enroll) {
 		enrollDao.insert(enroll);
 	}
-	
+
 	public void change(CourseEnrollment enroll) {
 		enrollDao.update(enroll);
 	}
-	
+
 	public void remove(String email) {
 		enrollDao.delete(email);
 	}
-	
+
 	public CourseEnrollment retrieveByEmail(String email) {
 		return enrollDao.selectOne(email);
 	}
-	
+
 	public List<CourseEnrollment> getEnrollmentList() {
 		return enrollDao.selectList();
 	}
-	
+
 	public void reject(String email) {
 		HashMap<String,Object> paramMap = new HashMap<>();
 		paramMap.put("email", email);
 		paramMap.put("status", CourseEnrollment.STATUS_REJECT);
-		
+
 		enrollDao.updateForStatus(paramMap);
 	}
-	
+
 	public void approve(String email) {
-		HashMap<String,Object> paramMap = new HashMap<>();
-		paramMap.put("email", email);
-		paramMap.put("status", CourseEnrollment.STATUS_APPROVE);
-		
-		enrollDao.updateForStatus(paramMap);
-		
-		CourseEnrollment enroll = enrollDao.selectOne(email);
+		DefaultTransactionDefinition txDef = new DefaultTransactionDefinition();
+		txDef.setName("t1");	
+		txDef.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		TransactionStatus txStatus = txManager.getTransaction(txDef);
+		try {
+			HashMap<String,Object> paramMap = new HashMap<>();
+			paramMap.put("email", email);
+			paramMap.put("status", CourseEnrollment.STATUS_APPROVE);
 
-		Student student = new Student();
-		student.setEmail(enroll.getEmail());
-		student.setName(enroll.getName());
-		student.setTel(enroll.getTel());
+			enrollDao.updateForStatus(paramMap);
 
-		studentService.register(student);
+			CourseEnrollment enroll = enrollDao.selectOne(email);
+
+			Student student = new Student();
+			student.setEmail(enroll.getEmail());
+			student.setName(enroll.getName());
+			student.setTel(enroll.getTel());
+
+			studentService.register(student);
+			txManager.commit(txStatus);
+
+		} catch (Exception e) {
+			// 예외가 발생한다면, 트랜잭션에 묶인 작업을 모두 취소한다.
+			txManager.rollback(txStatus);
+		}
 	}
 }
